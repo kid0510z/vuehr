15d912d1098ce52c518f7e1b6cc4401f8df33810
weiWeiSong <kid0510z@gmail.com>
2020-01-08 18:04:34 +0800

    权限组-1

--- src/components/sys/basic/RightsGroups.vue
+++ src/components/sys/basic/RightsGroups.vue
@@ -1,10 +1,31 @@
 <template>
-    <div>权限组</div>
+    <div>
+        <div>
+            <el-input placeholder="请输入英文名称" v-model="name" style="width: 300px;margin-right: 8px;">
+                <template slot="prepend">ROLE_</template>
+            </el-input>
+            <el-input v-model="nameZh" placeholder="请输入中文名称" style="width: 300px;margin-right: 8px;"></el-input>
+            <el-button type="primary">添加角色</el-button>
+        </div>
+        <div>
+
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "RightsGroups"
+        name: "RightsGroups",
+        data() {
+            return {
+                role: {
+                    name: '',
+                    nameZh: '',
+                }
+
+            }
+        }
+
     }
 </script>
 
